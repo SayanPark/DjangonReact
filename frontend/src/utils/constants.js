f271f1284d2d25670f6eca2c5ecf08755ceb270f
SayanPark <s.poormanesh2002@gmail.com>
2025-07-22 23:00:18 +0330

    modified:   frontend/src/utils/constants.js

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://backend-szkblog.trycloudflare.com/api/v1/";
-export const SERVER_URL = "https://backend-szkblog.trycloudflare.com";
+export const API_BASE_URL = "https://backend-szkblog.loca.lt/api/v1/";
+export const SERVER_URL = "https://backend-szkblog.loca.lt";
 export const CLIENT_URL = "https://sayanpark.github.io/DjangonReact/";
