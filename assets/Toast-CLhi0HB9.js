0f11e988f1a6b9ecfd5a3eac8cb080759bc40546
SayanPark <SayanPark@users.noreply.github.com>
2025-08-05 21:20:32 +0000

    deploy: b5a37b54aa30c8913515486e2183cc8e2a8c98d2

---
+++ assets/Toast-CLhi0HB9.js
@@ -0,0 +1,1 @@
+import{S as r}from"./index-DelESz2x.js";function e(t,o){return r.mixin({toast:!0,position:"top",showConfirmButton:!1,timer:1500,timerProgressBar:!0}).fire({icon:t,title:o})}export{e as default};
