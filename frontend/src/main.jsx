2d45b0a7c68cc2933b06b2fc36a4c6cf0652e982
SayanPark <s.poormanesh2002@gmail.com>
2025-08-09 20:37:33 +0330

    modified:   frontend/package-lock.json 	modified:   frontend/package.json 	modified:   frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,6 +6,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.css'
 import './App.css'
 import App from './App.jsx'
+import './i18n/index.js'
 import 'setimmediate';
 
 if (typeof window !== 'undefined' && !window.setImmediate) {
