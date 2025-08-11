66a779f9d83187dadda3c33e8350f64017540d60
SayanPark <s.poormanesh2002@gmail.com>
2025-08-12 00:26:21 +0330

    modified:   backend/backend/wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -17,6 +17,6 @@ project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
