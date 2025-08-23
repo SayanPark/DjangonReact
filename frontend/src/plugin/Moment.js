1cb11ac8b7d467e467da0809ec31b660a9bd330c
SayanPark <s.poormanesh2002@gmail.com>
2025-08-23 19:27:58 +0330

    modified:   frontend/src/plugin/Moment.js

--- frontend/src/plugin/Moment.js
+++ frontend/src/plugin/Moment.js
@@ -3,6 +3,6 @@ import moment from "moment-jalaali";
 moment.loadPersian({ dialect: "persian-modern" });
 
 function Moment(date) {
-    return moment(date).format("jD / jMM / jYYYY");
+    return moment(date).format("jDD / jMM / jYYYY");
 }
 export default Moment;
